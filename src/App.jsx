d1636cfe16a8cac21dad04749ab3fc0bcfbc04c8
Qasimm-Ali <qqali776655@gmail.com>
2024-06-10 19:59:40 +0500

    Main page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Buttons from "./Components/Buttons";
+import NoProjects from "./Components/NoProjects";
 import Sidebar from "./Components/Sidebar";
 
 export default function App(){
   return(<>
   <main className="flex h-screen ">
   <Sidebar/>
+  <NoProjects/>
   </main>
   </>)
 }
