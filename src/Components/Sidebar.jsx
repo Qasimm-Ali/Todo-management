738f07a3c83185291223242f59841a1535bdb47d
Qasimm-Ali <qqali776655@gmail.com>
2024-06-15 20:47:09 +0500

    Added some styles

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,22 +1,28 @@
 import Buttons from "./Buttons";
 
-export default function Sidebar({onClick, listOfProjects}){
+export default function Sidebar({onClick, listOfProjects, onSelect, selectedProjectId}){
+
     return(<>
-    <aside className="w-[40%] bg-stone-900 py-12 text-stone-300 my-5 rounded-r-md   ">
-        <div className=" px-12">
+    <aside className="w-[30%] px-12 bg-stone-900 py-12 text-stone-300 my-5 rounded-r-md   ">
+        <div >
             <p className="font-medium text-2xl "> Your Projects </p>
             <div className="pt-4">
             <Buttons onClick={onClick}>+ Add Project </Buttons>
             </div>
         </div>
-        <ul className="mt-7 w-[90%] px-2  ">
-            {listOfProjects.map((item, ind) => (<>
+        <ul className="mt-7 px-2 ">
+            {listOfProjects.map((item, ind) => {
+                let cssClasses = "w-full text-left mt-4 px-2 py-1 rounded-sm text-stone-400 hover:text-stone-200  "
+                if(item.id === selectedProjectId){
+                    cssClasses += "bg-stone-800 text-stone-300" 
+                }
+                return(
                 <li key={ind} className=" min-h-7 ">  
-                    <span> {item.title} </span>
-                    <span className="float-right"> {item.dueDate} </span>
+                    <button className={cssClasses} > {item.title} </button> 
                 </li>
-            </>
-            ))}
+                )
+            }
+            )}
         </ul>
     </aside>
     </>)
